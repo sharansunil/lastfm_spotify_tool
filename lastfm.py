ba6c6b2f811d2e4d724ce1a8c2b88467e7ec5879
sharansunil <sharansnl@gmail.com>
2018-09-25 18:39:25 +0800

    v1.3x

--- lastfm.py
+++ lastfm.py
@@ -42,7 +42,6 @@ def fixTime(test):
 	test = t+test
 	test = test.strftime('%H:%M')
 	return test
-print("A")
 def generateRecentTrackSet(n=500):
 	df = list(get_recent_tracks(username, n))
 	df = pd.DataFrame(df, columns=['trackartist', 'album', 'datetime', 'rand'])
@@ -66,3 +65,26 @@ def generateRecentTrackSet(n=500):
 	df.to_csv('exports/recentTracks.csv')
 	return df
 
+
+def topAlbumsArtists():
+	user = network.get_user(username)
+	topArtists = user.get_top_artists(limit=100)
+	topAlbums = user.get_top_albums(limit=100)
+	ArtistPlays=[]
+	AlbumPlays=[]
+
+	for obj in topArtists:
+		sx = obj.item
+		sx = sx.get_name()
+		plays = obj.weight
+		ArtistPlays.append([sx, plays])
+	df1=pd.DataFrame(ArtistPlays,columns=["Artist","Plays"])
+	df1.to_csv('exports/TopArtist.csv')
+	for obj in topAlbums:
+		sx = obj.item
+		sx = sx.get_name()
+		plays = obj.weight
+		AlbumPlays.append([sx, plays])
+	df2 = pd.DataFrame(AlbumPlays, columns=["Album", "Plays"])
+	df2.to_csv("exports/TopAlbums.csv")
+	return [df1,df2]
