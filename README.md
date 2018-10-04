47bec47a4b64777d50ab8592be1c3380385f45be
sharansunil <sharansnl@gmail.com>
2018-10-04 11:12:02 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # spotify+lastfm integration and visualization tool
 
-This is a project/tool to integrate spotify and lastfm api's to get a cohesive view of your Spotify library.
+##This is a project/tool to integrate spotify and lastfm apis' to get a summary view of key info in your Spotify+LastFM libraries.
 
-The code is split into multiple files. Helper functions for individual api's can be found in their distinct python files, named accordingly. 
+###TODO
+- Build unit and integration tests
+- Create website
+- Create a more secure way of storing data
 
-Classes for credential and generator loading can be found in BaseClasses.
+###What does each python file do?
+- For readability, the files were split into functions and classes.
+- BaseClasses contains information to generate the classes. 
+- LastFmFunctions and SpotifyFunctions do what the names say. 
+- Execution is handled only at run.py. 
 
-For simple execution and obtaining various csv's and dataframes to work with, simply use run.py. The only inputs required will be your credentials and any settings you wish to toggle.
+###I found a bug?
+Submit an issue
+
+###I want to add something new. 
+Submit a pull request
+
+###Please feel free to give feedback and advice about my code be it optimisation, cleaning or more features!
--- run.py
+++ run.py
@@ -8,27 +8,25 @@ sp_username = '1177566421'
 lastfm_username = "sharansunil"
 
 
-"""creates main object to generate datasets and plots. all binary variables default to 1. scope is already set to user-library-read.
-set all variables to 1 if generating for the first time"""
-main = Spotify_LastFM_Builder(	
+"""creates object to generate datasets and plots. all binary variables default to 1. scope is preset to user-library-read.set all variables to 1 if generating for the first time"""
+spotify_fm = Spotify_LastFM_Builder(	
 	lastfm_username=lastfm_username, 
 	sp_username=sp_username
 )
 
 """set password by input instance or fix it in code, up to you"""
-main.setPassword("synystrax")
+spotify_fm.setPassword("synystrax")
 
 """update dataset"""
-main.update_datasets(
+spotify_fm.update_datasets(
 	##spotify refreshers - recommended to set at 0
 	refresh_spotify=0,
-	refresh_artist_viz=1,
+	refresh_artist_viz=0,
 	refresh_playlist_viz=0,
 	##lastfm refreshers - recommended to set at 1
 	lastfm_artistalbum=1,
 	lastfm_tracks=1
 	)
 
-"""	load datasets. if both options set to 1 will return as dictionary with playlist and tracks as keys
-	default state: """
-df=main.load_datasets(playlist=1,tracks=1)
+"""	load datasets. if both options set to 1 will return as dictionary with playlist and tracks as keys. default state: 1,1"""
+dataset_dictionary=spotify_fm.load_datasets(playlist=1,tracks=1)
