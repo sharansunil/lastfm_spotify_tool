69cdfd69022a91b2bb028cad9271afd870aba4a9
sharansunil <sharansnl@gmail.com>
2018-12-03 15:45:12 +0800

    fixed uri

--- run.py
+++ run.py
@@ -12,15 +12,14 @@ spotify_fm = Spotify_LastFM_Builder(
 
 spotify_fm.update_datasets(
     # spotify refreshers - recommended to set at 0
-    refresh_spotify=0,
-    refresh_artist_viz=0,
-    refresh_playlist_viz=0,
+    refresh_spotify=1,
+    refresh_artist_viz=1,
+    refresh_playlist_viz=1,
     # lastfm refreshers - recommended to set at 1
     lastfm_artistalbum=1,
     lastfm_tracks=1,
     refresh_gsheet=1
 )
-
 
 retdict = spotify_fm.load_datasets()
 #genius lyric pull
